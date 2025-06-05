582f5c2d6b105c0a2dcfd2b76503331ed3407f36
Robert <ryau91@gmail.com>
2025-06-05 18:33:12 +0100

    Updated burger menu to fixed

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -16,14 +16,15 @@ const Sidenav = () => {
 
   return (
     <div>
-      <AiOutlineMenu size={30}
+      <AiOutlineMenu
+        size={30}
         onClick={handleNav}
-        className="absolute top-4 right-4 z-[99] md:hidden"
+        className="fixed top-4 right-4 z-[99] md:hidden"
       />
       {/* if 'nav' is true, show this */}
       {nav ? (
         <div className="fixed w-full h-screen bg-yellow-100/90 flex flex-col justify-center items-center z-20">
-          <a 
+          <a
             onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
