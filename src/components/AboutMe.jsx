8f8c218f7de808080c2e0c83fa902de2528adb13
Robert <ryau91@gmail.com>
2025-06-05 23:46:13 +0100

    Updated about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
         About Me
       </h1>
       <h3 className="flex justify-center p-4">
-      Passionate programmer for many years. Recently completed a web development course, honing expertise in a spectrum of technologies including HTML, CSS, Javascript, React, and more. Eager to leverage this comprehensive skill set within a dynamic team, contributing to the creation of innovative web solutions. Seeking a fulfilling career in web development to channel creativity, problem-solving prowess, and collaborative spirit into impactful projects.
+      I am a passionate programmer with many years of experience. I have completed and contributed to a few projects, helping me to stay up to date with the newest technologies. Now, I am keen on further applying my skills in a dynamic team to help create innovative web solutions. I am looking for a rewarding career in web development where I can make a meaningful impact by using my creativity, and problem-solving skills.
       </h3>
     </div>
   )
